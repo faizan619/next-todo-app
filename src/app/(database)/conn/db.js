ec99f4a48230c48f7e40a510345809c8f0b6e288
faizan619 <alamf6023@gmail.com>
2024-01-25 22:32:13 +0530

    added database config

--- src/app/(database)/conn/db.js
+++ src/app/(database)/conn/db.js
@@ -1,11 +1,2 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    try{
-        await mongoose.connect(process.env.MONGO_DB_URL)
-        console.log("Connected With Database")
-
-    } catch (error) {
-        console.log("Cannot Connect with Database : ",error)
-    }
-}
