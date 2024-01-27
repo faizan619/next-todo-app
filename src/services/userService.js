ef22ba4aedf7854b36fdaa890a15ab585a39333b
faizan619 <alamf6023@gmail.com>
2024-01-27 18:20:23 +0530

    added login service

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,3 +4,8 @@ export async function RegisterSer(user){
     const result = await httpAxios.post("/api/users",user).then((response)=>response.data)
     return result
 }
+
+export async function login(LoginData){
+    const result = await httpAxios.post('/api/login',LoginData).then((response)=>response.data)
+    return result;
+}
